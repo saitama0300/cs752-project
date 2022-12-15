013ee1500251e1dca71ccbbeb1c5085e4e4a5da6
Aditya Jain <adityajain.aj10@gmail.com>
2022-12-15 11:24:04 -0600

    matching sin kernel for nerf now

--- testkit/model.py
+++ testkit/model.py
@@ -4,6 +4,12 @@ import cudaprofile
 import os
 import sys
 
+# Hyper Parameters
+NUM_FEATURES = 2048 * 8 * 4
+HIDDEN_LAYER_SIZE = 1024 * 16
+
+#
+
 CASIO=os.environ.get('CASIO')
 sys.path.append(f'{CASIO}/utils')
 import cudaprofile
@@ -12,19 +18,20 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 class Feedforward(torch.nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size ):
         super(Feedforward, self).__init__()
         self.input_size = input_size
-        self.hidden_size  = hidden_size
-        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.fc1 = torch.nn.Linear(self.input_size, 1)
         self.relu = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
         self.sigmoid = torch.nn.Sigmoid()
+
     def forward(self, x):
-        hidden = self.fc1(x)
-        relu = self.relu(hidden)
-        output = self.fc2(relu)
-        output = self.sigmoid(output)
+        # We need to perform sin multiple times, so that it shows
+        # up in the sampling.     
+        for _ in range(100):
+            x = torch.sin(x)
+        output = self.fc1(x)
+        output = self.sigmoid(hidden)
         return output
 
 from sklearn.datasets import make_blobs
@@ -34,16 +41,16 @@ def blob_label(y, label, loc): # assign labels
         target[y == l] = label
     return target
 
-x_train, y_train = make_blobs(n_samples=40, n_features=2, cluster_std=1.5, shuffle=True)
+x_train, y_train = make_blobs(n_samples=10, n_features=NUM_FEATURES, cluster_std=1.5, shuffle=True)
 x_train = torch.FloatTensor(x_train).to(device)
 y_train = torch.FloatTensor(blob_label(y_train, 0, [0])).to(device)
 y_train = torch.FloatTensor(blob_label(y_train.cpu(), 1, [1,2,3])).to(device)
-x_test, y_test = make_blobs(n_samples=10, n_features=2, cluster_std=1.5, shuffle=True)
+x_test, y_test = make_blobs(n_samples=10, n_features=NUM_FEATURES, cluster_std=1.5, shuffle=True)
 x_test = torch.FloatTensor(x_test).to(device)
 y_test = torch.FloatTensor(blob_label(y_test, 0, [0])).to(device)
 y_test = torch.FloatTensor(blob_label(y_test.cpu(), 1, [1,2,3])).to(device)
 
-model = Feedforward(2, 10).to(device)
+model = Feedforward(NUM_FEATURES, HIDDEN_LAYER_SIZE).to(device)
 criterion = torch.nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
@@ -53,9 +60,11 @@ before_train = criterion(y_pred.squeeze(), y_test)
 print('Test loss before training' , before_train.item())
 
 model.train()
-epoch = 20
-for epoch in range(epoch):
-    cudaprofile.start()
+total_epochs = 35
+warmup = 30
+for epoch in range(total_epochs):
+    if epoch == warmup:
+        cudaprofile.start()
     optimizer.zero_grad()
     # Forward pass
     y_pred = model(x_train)
@@ -63,10 +72,12 @@ for epoch in range(epoch):
     loss = criterion(y_pred.squeeze(), y_train)
    
     print('Epoch {}: train loss: {}'.format(epoch, loss.item()))
+    sys.stdout.flush()
     # Backward pass
     loss.backward()
     optimizer.step()
-    cudaprofile.stop()
+    if epoch == total_epochs - 1: 
+        cudaprofile.stop()
 
 model.eval()
 y_pred = model(x_test)
